ee9488952feee612ebb52fdace74b5996277daff
Peter Hallam <pragmatic@cpan.org>
2011-12-22 18:33:09 +0800

    Initial commit with dzil new

---
+++ lib/WebService/Pingdom.pm
@@ -0,0 +1,29 @@
+#  COPYRIGHT: © 2011 Peter Hallam.
+
+package WebService::Pingdom;
+
+#   ABSTRACT: This module does
+#    CREATED: Thu, 22 Dec 2011 10:33:09 UTC
+#     AUTHOR: Peter Hallam <pragmatic@cpan.org>
+
+use strict;
+use warnings;
+use v5.10.1;
+
+# VERSION
+
+
+
+1;
+__END__
+=pod
+
+=head1 SYNOPSIS
+
+ use WebService::Pingdom;
+
+ my $pingdom = WebService::Pingdom->new;
+
+=cut
+
+# vim:set ts=4 sw=4 et ft=perl:
